65ed4889ef852db3b68b4165b0e45fdc8c099f83
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-06-07 15:04:29 -0700

    add timezone and thousands separator

--- evergreen_financial_app/settings.py
+++ evergreen_financial_app/settings.py
@@ -125,19 +125,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Canada/Pacific'
 USE_I18N = True
-
 USE_TZ = True
+USE_L10N = True
+
+USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -147,7 +146,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
