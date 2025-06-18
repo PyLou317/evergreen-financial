3670628c6019008501a76d0a156bcd9b4ee7cb9f
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-06-17 21:00:48 -0700

    add category urls

--- category/urls.py
+++ category/urls.py
@@ -5,5 +5,8 @@ from .views import *
 app_name = 'category'
 
 urlpatterns = [
-
+    path('categories/', CategoryList.as_view(), name='categories'),
+    
+    # API's
+    path('categories-api', CategoryListAPIView.as_view(), name='categories-api'),
 ]
