97a1379f047146bb386e813bda7e15e2ee6aaf8a
Lucas Patriquin <lucas.patriquin@gmail.com>
2025-07-22 21:03:36 -0700

    Order Category field by name

--- transactions/forms.py
+++ transactions/forms.py
@@ -14,3 +14,7 @@ class TransactionForm(forms.ModelForm):
         widgets = {
             'notes': forms.Textarea(attrs={'rows': 2, 'cols': 40}),
         }
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].queryset = Category.objects.order_by('name')
